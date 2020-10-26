4dc1e892dcf94ae8aa53022b3d0786133d09067c
Vinicius Martins <vinimartdev@gmail.com>
2020-10-26 16:04:34 -0300

    Initial commit

---
+++ section-3-introducao/variaveis.c
@@ -0,0 +1,13 @@
+// Aula sobre variáveis
+#include <stdio.h>
+
+int main()
+{
+    // Declarando e inicializando variáveis:
+    // Declaração
+    int idade;
+
+    printf('Qual a sua idade?');
+
+    return 0;
+}
